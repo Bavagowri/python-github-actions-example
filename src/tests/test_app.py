8d338789c3606458c2d15f84a9f7f50314e047ca
Bavagowri <bavagowri.s1@gmail.com>
2023-03-30 08:34:47 +0530

    Deleted unwanted file

--- src/tests/test_app.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import index
-
-def test_index():
-    assert index() == "hello world!"
